563d88ab63095e03af0d4514a35289f31fb7d141
JackHillman <jack.hillman@me.com>
2015-10-10 08:41:19 +1030

    Basic Start

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
-
-
-
+	
+	var firstClick = true;
+	
+	function hide()
+	{
+		if (firstClick === true)
+		{
+			$(".hidden").css("opacity", "0.4");
+			$(".hidden2").css("top", "0");
+			$(".hidden2").css("height", "100vh");
+			$(".hidden2").css("width", "300px");
+		}
+		else
+		{
+			$(".hidden").css("opacity", "0");
+			$(".hidden2").css("top", "-100px");
+			$(".hidden2").css("height", "100px");
+			$(".hidden2").css("width", "100px");
+		}
+		firstClick = !firstClick;
+	}
+	
+	$("button").click(hide);
+	
 });
